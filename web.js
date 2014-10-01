2f5bbd37a619ad627f658c1ab8d6d32dcefc1167
koroket <mikebando@mikes-MacBook-Pro.local>
2014-10-01 14:48:46 -0700

    post3

--- web.js
+++ web.js
@@ -1,15 +1,45 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
-var app = express();
+var mongo = require('mongodb');
 
+var app = express();
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/mydb';
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('mydocs', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    });
+  });
 });
+
+app.get('/collections/candys', function(req, res) {
+  var collection = mongo.db.collection("candy")
+
+  collection.find({} ,{}).toArray(function(e, results){
+    if (e) res.status(500).send()
+    res.send(results)
+  })
+
+})
+
+app.post('/collections/candys', function(req, res) {
+  var collection = mongo.db.collection("candy")
+
+  collection.insert(req.body, {}, function(e, results){
+    if (e) res.status(500).send()
+    res.send(results) 
+  })
+})
+
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
