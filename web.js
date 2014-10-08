9f2456c5217fe0e400f058989d7e60325d8d73c7
koroket <mikebando@mikes-MacBook-Pro.local>
2014-10-07 21:27:41 -0700

    server update 1

--- web.js
+++ web.js
@@ -100,7 +100,7 @@ var variable = str3.concat(str4);
     action
   }, {safe: true, multi: false}, function(e, result){
     if (e) res.status(500).send()
-    console.log(result)
+    res.send((result === 1)?result : {msg: 'error'})
   })
 })
 
