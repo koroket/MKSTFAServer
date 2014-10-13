6eb81619f70ac73bd9f1a2131dfc384303bb60d8
koroket <mikebando@mikes-MacBook-Pro.local>
2014-10-13 11:48:41 -0700

    server 26

--- web.js
+++ web.js
@@ -69,17 +69,7 @@ app.post('/groups', function(req, res) {
   })
 })
 app.get('/token/:token', function(req, res) {
-  var myDevice = new apn.Device(:token);
 
-    var note = new apn.Notification();
-
-note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
-note.badge = 3;
-note.sound = "ping.aiff";
-note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
-note.payload = {'messageFrom': 'Caroline'};
-
-apnConnection.pushNotification(note, myDevice);
 })
 
 app.get('/groups', function(req, res) {
