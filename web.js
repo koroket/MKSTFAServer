064b16bfd91ce56bf9cfda22ee70664e5f4fafb5
koroket <bmasakazu@gmail.com>
2014-10-19 23:32:57 -0700

    yeah

--- web.js
+++ web.js
@@ -49,11 +49,14 @@ app.get('/ppl/:friend', function(req, res) {
 })
 app.post('/token/:friend', function(req, res) {
   var collection = db.collection(req.params.friend)
-  console.log(collection);
-  collection.insert(req.body, {}, function(e, results){
+  console.log(collection.length);
+  if(collection.length<1)
+  {
+     collection.insert(req.body, {}, function(e, results){
     if (e) res.status(500).send()
-    res.send(results) 
-  })
+    res.send(collection) 
+    })
+  }
 })
 
 app.get('/token/:friend', function(req, res) {
