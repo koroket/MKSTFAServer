8a7c839b393b55d1a8ab7a4e4a6ce07ef4334b17
Ares42 <luke_solomon@me.com>
2014-10-30 10:37:58 -0700

    no message

--- web.js
+++ web.js
@@ -1,6 +1,4 @@
-var express = require('express'),
-  mongoskin = require('mongoskin'),
-  bodyParser = require('body-parser')
+var express = require('express'), mongoskin = require('mongoskin'), bodyParser = require('body-parser');
 var logfmt = require("logfmt");
 var mongo = require('mongodb');
 var apn = require('apn');
@@ -12,148 +10,145 @@ var yelp = require("yelp").createClient({
 });
 var GooglePlaces = require("googleplaces");
 var googlePlaces = new GooglePlaces("AIzaSyC5p7KH-SOmf2dgYFMqFm9H1vYAXX0jcMs", "json");
-
-
 var options = { };
-
 var apnConnection = new apn.Connection(options);
-
-var app = express()
-app.use(bodyParser())
-
-var mongoUri = process.env.MONGOLAB_URI ||
-  process.env.MONGOHQ_URL ||
-  'mongodb://localhost/mydb';
-
+var app = express() 
+  app.use(bodyParser())
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL ||'mongodb://localhost/mydb';
 var db = mongoskin.db(mongoUri, {safe:true})
 
-app.param('collectionName', function(req, res, next, collectionName){
+app.param('collectionName', function(req, res, next, collectionName) {
   req.collection = db.collection(collectionName)
   return next()
 })
 
+// ==============================================================================================
+// Post
+// ==============================================================================================
 app.post('/ppl/:friend', function(req, res) {
   var collection = db.collection(req.params.friend)
-
   collection.insert(req.body, {}, function(e, results){
     if (e) res.status(500).send()
     res.send(results) 
   })
 })
 
-app.get('/ppl/:friend', function(req, res) {
-  var collection = db.collection(req.params.friend)
-
-  collection.find({} ,{}).toArray(function(e, results){
-    if (e) res.status(500).send()
-    res.send(results)
-  })
-
-})
 app.post('/token/:friend', function(req, res) {
   var collection = db.collection(req.params.friend)
   collection.count({}, function(error, numOfDocs) {
-    if(numOfDocs<1)
-    {
-        collection.insert(req.body, {}, function(e, results){
-
-        if (e) res.status(500).send()
-        res.send(results) 
-
-        })  
+    if(numOfDocs<1) {
+      collection.insert(req.body, {}, function(e, results) {
+      if (e) res.status(500).send()
+      res.send(results) 
+      })  
     }
-    else
-    {
-        collection.find().toArray(function(err, docs) {
-        res.send(docs);
-        });
+    else {
+      collection.find().toArray(function(err, docs) {
+      res.send(docs);
+      });
     }
   })
+})
 
+app.post('/groups', function(req, res) {
+  var collection = db.collection('groups')
+  collection.insert(req.body, {}, function(e, results) {
+    if (e) res.status(500).send()
+    res.send(results) 
+  })
+})
+
+app.post('/token/push/:token/:daindex/:groupid', function(req, res) {
+  var myDevice = new apn.Device(req.params.token);
+  var note = new apn.Notification();
+  note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+  note.badge = 3;
+  note.sound = "ping.aiff";
+  note.alert = "\uD83D\uDCE7 \u2709 You have a new group invite";
+  note.payload = req.body;
+  apnConnection.pushNotification(note, myDevice);
+})
+
+// ==============================================================================================
+// Put
+// ==============================================================================================
+
+app.put('/groups/:id/:number', function(req, res, next) {
+  var collection = db.collection('groups')
+  var str1 = "Replies.";
+  var str2 = req.params.number;
+  var variable = str1.concat(str2);
+  var action = {};
+  action[variable] = 1;
+  collection.updateById(req.params.id, {$inc: action}, {safe: true, multi: false}, function(e, result){
+      if (e) res.status(500).send()
+      collection.findById(req.params.id, function(e2, result2){
+        if (e2) res.status(500).send()
+          console.log(result2.Replies[req.params.number]);
+        res.send({NumberOfReplies:result2.Replies[req.params.number] 
+      })
+    })
+  })
+})
+
+app.put('/groups/:id/:number/finished', function(req, res, next) {
+  var collection = db.collection('groups')
+  var str1 = "Done";
+  var action = {};
+  action[variable] = req.params.number;
+  collection.updateById(req.params.id, {$set: action}, {safe: true, multi: false}, function(e, result) {
+    if (e) res.status(500).send()
+    collection.findById(req.params.id, function(e2, result2) {
+      if (e2) res.status(500).send()
+        console.log(result2.Replies[req.params.number]);
+        res.send({NumberOfReplies:result2.Replies[req.params.number] 
+      })
+    })
+  })
+})
+
+// ==============================================================================================
+// Get
+// ==============================================================================================
+app.get('/ppl/:friend', function(req, res) {
+  var collection = db.collection(req.params.friend)
+  collection.find({} ,{}).toArray(function(e, results) {
+    if (e) res.status(500).send()
+    res.send(results)
+  })
 })
 
 app.get('/token/:friend', function(req, res) {
   var collection = db.collection(req.params.friend)
-
-  collection.find({} ,{}).toArray(function(e, results){
+  collection.find({} ,{}).toArray(function(e, results) {
     if (e) res.status(500).send()
     res.send(results)
   })
-
 })
 
 app.get('/yelp/:location/:search/:mynum', function(req, res) {
-  
   yelp.search({limit: req.params.mynum, location: req.params.location, term:req.params.search}, function(error, data) {
   if(error) res.status(500).send()
     res.send(data)
-});
-  
-
+  });
 })
 
-
-app.get('/google/:search', function(req, res) {
-  
 /**
  * Place search - https://developers.google.com/places/documentation/#PlaceSearchRequests
  */
-    var parameters;
-
-    parameters = {
-        location:[40.67, -73.94],
-        types: req.params.search
-    };
-
-    googlePlaces.placeSearch(parameters, function (response) {
-  googlePlaces.placeDetailsRequest({reference:response.results[0].reference}, function (response) {
-    res.send(response.result)
+app.get('/google/:search', function(req, res) {
+  var parameters;
+  parameters = {location:[40.67, -73.94], types: req.params.search};
+  googlePlaces.placeSearch(parameters, function (response) {
+    googlePlaces.placeDetailsRequest( {reference:response.results[0].reference}, function (response) {
+      res.send(response.result)
+    });
   });
-});
-  
-
-})
-
-
-app.post('/groups', function(req, res) {
-  var collection = db.collection('groups')
-
-  collection.insert(req.body, {}, function(e, results){
-    if (e) res.status(500).send()
-    res.send(results) 
-  })
-})
-app.get('/token/push/:token/:daname', function(req, res) {
-  var myDevice = new apn.Device(req.params.token);
-
-    var note = new apn.Notification();
-
-note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
-note.badge = 0;
-note.sound = "ping.aiff";
-note.alert = "\uD83D\uDCE7 \u2709 You have a new group invite";
-note.payload = {'messageFrom': req.params.daname, 'type': "message"};
-
-apnConnection.pushNotification(note, myDevice);
-})
-app.post('/token/push/:token/:daindex/:groupid', function(req, res) {
-  var myDevice = new apn.Device(req.params.token);
-
-    var note = new apn.Notification();
-
-note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
-note.badge = 3;
-note.sound = "ping.aiff";
-note.alert = "\uD83D\uDCE7 \u2709 You have a new group invite";
-note.payload = req.body;
-
-apnConnection.pushNotification(note, myDevice);
 })
 
 app.get('/groups', function(req, res) {
   var collection = db.collection('groups')
-
-  collection.find({} ,{}).toArray(function(e, results){
+  collection.find({} ,{}).toArray(function(e, results) {
     if (e) res.status(500).send()
     res.send(results)
   })
@@ -161,82 +156,49 @@ app.get('/groups', function(req, res) {
 
 app.get('/groups/:id', function(req, res) {
   var collection = db.collection('groups')
-
-  collection.findById(req.params.id, function(e, result){
+  collection.findById(req.params.id, function(e, result) {
     if (e) res.status(500).send()
     res.send(result)
   })
 })
 
-app.put('/groups/:id/:number', function(req, res, next) {
-
-  var collection = db.collection('groups')
-
-  var str1 = "Replies.";
-  var str2 = req.params.number;
-  var variable = str1.concat(str2);
-  
- var action = {};
-
- action[variable] = 1;
-  collection.updateById(req.params.id, {$inc:
-    action
-  }, {safe: true, multi: false}, function(e, result){
-    if (e) res.status(500).send()
-    collection.findById(req.params.id, function(e2, result2){
-      if (e2) res.status(500).send()
-        console.log(result2.Replies[req.params.number]);
-      res.send({NumberOfReplies:result2.Replies[req.params.number] }
-
-        )
-    })
-  })
+app.get('/token/push/:token/:daname', function(req, res) {
+  var myDevice = new apn.Device(req.params.token);
+  var note = new apn.Notification();
+  note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+  note.badge = 0;
+  note.sound = "ping.aiff";
+  note.alert = "\uD83D\uDCE7 \u2709 You have a new group invite";
+  note.payload = {'messageFrom': req.params.daname, 'type': "message"};
+  apnConnection.pushNotification(note, myDevice);
 })
-app.put('/groups/:id/:number/finished', function(req, res, next) {
 
-  var collection = db.collection('groups')
-
-  var str1 = "Done";
-
-  
- var action = {};
-
- action[variable] = req.params.number;
-  collection.updateById(req.params.id, {$set:
-    action
-  }, {safe: true, multi: false}, function(e, result){
-    if (e) res.status(500).send()
-    collection.findById(req.params.id, function(e2, result2){
-      if (e2) res.status(500).send()
-        console.log(result2.Replies[req.params.number]);
-      res.send({NumberOfReplies:result2.Replies[req.params.number] }
-
-        )
-    })
-  })
-})
+// ==============================================================================================
+// Delete
+// ==============================================================================================
 app.delete('/groups/:id', function(req, res, next) {
   var collection = db.collection('groups')
-  collection.removeById(req.params.id, function(e, result){
+  collection.removeById(req.params.id, function(e, result) {
     console.log(result)
     if (e) return next(e)
     res.send((result === 1)?{msg: 'success'} : {msg: 'error'})
   })
 })
+
 app.delete('/ppl/:friend/:id', function(req, res) {
   var collection = db.collection(req.params.friend)
-
-  collection.removeById(req.params.id, function(e, result){
+  collection.removeById(req.params.id, function(e, result) {
     console.log(result)
     if (e) return next(e)
-    res.send((result === 1)?{msg: 'success'} : {msg: 'error'})
+      res.send((result === 1)?{msg: 'success'} : {msg: 'error'})
   })
-
 })
 
+// ==============================================================================================
+// Port
+// ==============================================================================================
 var port = Number(process.env.PORT || 5000);
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-
