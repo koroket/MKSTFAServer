d4eafc2569087cffd7c4d4d0dc0f516b5c71ca45
koroket <bmasakazu@gmail.com>
2014-11-02 01:30:45 -0800

    m

--- web.js
+++ web.js
@@ -104,13 +104,13 @@ app.get('/ppl/:fbid/paypal/:ppid', function(req, res) {
           collection.updateById(results[0]._id,{$set:{"paypal": req.params.ppid}},{safe: true, multi: false}, function(e3, result3){
           if(e3) res.status(500).send()
           console.log("saved paypal")
-          res.send(result3)
+          res.send("paypal is linked")
           })
         }
         else
         {
           console.log("has pyapal already")
-          res.send("cant change it sorry :P")
+          res.send("cant change it")
         }
     }
   })
