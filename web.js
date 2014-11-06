76a5d36d45032354b1667d73a92d01d011302199
koroket <bmasakazu@gmail.com>
2014-11-06 14:04:37 -0800

    m

--- web.js
+++ web.js
@@ -10,6 +10,8 @@ var yelp = require("yelp").createClient({
   token: "PMSIKP0XrmmaqoCzHjwRB9K3DM4oIDOf",
   token_secret: "KRSvWPtiHBp-NLmfz8xeArwKDZ0"
 });
+var request = require('request');
+var cheerio = require('cheerio');
 
 var async = require('async')
 
@@ -399,6 +401,35 @@ app.get('/yelp/:lat/:longi/:search/:offset', function(req, res) {
       temp["rating"] = infoDictionary["rating"]
     }
     decisionObjects.push(temp)
+
+    url = infoDictionary["url"];
+
+  request(url, function(error, response, html){
+    if(!error){
+      var $ = cheerio.load(html);
+
+      var title, release, rating;
+      var json = { title : "", release : "", rating : ""};
+
+            // We'll use the unique header class as a starting point.
+
+      $('.attribute-key').filter(function(){
+
+           // Let's store the data we filter into a variable so we can easily see what's going on.
+
+            var data = $(this);
+
+           // In examining the DOM we notice that the title rests within the first child element of the header tag. 
+           // Utilizing jQuery we can easily navigate and get the text by writing the following code:
+
+            console.log(data.text())
+
+           // Once we have our title, we'll store it to the our json object.
+
+           // json.title = title;
+          })
+    }
+  })
   }
 
   res.send(decisionObjects)
