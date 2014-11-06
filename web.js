4ab223f03aaea28b2fe0be6d48aa6118816ede44
koroket <bmasakazu@gmail.com>
2014-11-06 15:25:08 -0800

    m

--- web.js
+++ web.js
@@ -428,7 +428,7 @@ app.get('/yelp/:lat/:longi/:search/:offset', function(req, res) {
 
            // json.title = title;
       })
-      $('dd.nowrap-price-description').filter(function(){
+      $('dd.class.nowrap-price-description').filter(function(){
 
            // Let's store the data we filter into a variable so we can easily see what's going on.
 
